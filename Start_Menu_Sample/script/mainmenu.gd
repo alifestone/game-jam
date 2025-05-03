f61e6fee76a41888b9584117ef98c681a4c72df0
kevin101094 <88360083+shiraga1094@users.noreply.github.com>
2025-05-03 23:08:27 +0800

    Sample

---
+++ Start_Menu_Sample/script/mainmenu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("Menu_Quit"):
+		get_tree().quit()
+	if Input.is_action_pressed("Menu_Start"):
+		print("Game play.")
+		#Call Game to Run
+		pass
