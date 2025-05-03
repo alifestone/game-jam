4073ac05600edaba91d079f2b3c4360bb25f673b
CSQ <csqsaysuwu@gmail.com>
2025-05-04 02:07:13 +0800

    New commit

--- scripts/StartMenu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://Main.tscn")
-
-func _on_quit_button_pressed():
-	get_tree().quit()
---
+++ scripts/coin.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+@onready var game_manager: Node = %GameManager
+
+
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
+	print("+1 coin")
+	queue_free()
